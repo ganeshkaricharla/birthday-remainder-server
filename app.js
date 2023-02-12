5e0ab207c1dc1be5923520d54e2aba859a296515
Karicharla GNV Swamy Naidu <57288392+ganeshkaricharla@users.noreply.github.com>
2023-02-12 12:14:50 +0530

    Added some comments

--- app.js
+++ app.js
@@ -1,51 +1,58 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser')
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const app = express();
-let cors = require('cors')
-app.use(cors())
+let cors = require("cors");
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
-mongoose.connect('mongodb://localhost:27017/birthdayDB', { useNewUrlParser: true }, () => { console.log("connected to db") })
+{
+  /** It connects to mongod,  */
+}
+mongoose.connect(
+  "mongodb://localhost:27017/birthdayDB",
+  { useNewUrlParser: true },
+  () => {
+    console.log("connected to db");
+  }
+);
 const birthDaySchema = mongoose.Schema({
-    fullname: String,
-    words: String,
-    relation: String,
-    gender: String,
-    showYear: Boolean,
-    month: Number,
-    year: Number,
-    day: Number,
-
+  fullname: String,
+  words: String,
+  relation: String,
+  gender: String,
+  showYear: Boolean,
+  month: Number,
+  year: Number,
+  day: Number,
 });
-const Birthday = mongoose.model('birthday', birthDaySchema);
+const Birthday = mongoose.model("birthday", birthDaySchema);
 app.get("/all", function (req, res) {
-    Birthday.find({}, function (err, foundItems) {
-        res.json(foundItems)
-    })
-})
+  Birthday.find({}, function (err, foundItems) {
+    res.json(foundItems);
+  });
+});
 
 app.post("/add", function (req, res) {
-    const date = new Date(req.body.dob);
-    var showyear = req.body.showYear === "Yes" ? true : false;
-    const birthday = new Birthday({
-        fullname: req.body.fullname,
-        relation: req.body.relation,
-        gender: req.body.gender,
-        words: req.body.description,
-        showYear: showyear,
-        month: date.getMonth(),
-        day: date.getDate(),
-        year: date.getFullYear()
-    });
-    console.log(birthday)
-    birthday.save();
-    res.end();
-})
+  const date = new Date(req.body.dob);
+  var showyear = req.body.showYear === "Yes" ? true : false;
+  const birthday = new Birthday({
+    fullname: req.body.fullname,
+    relation: req.body.relation,
+    gender: req.body.gender,
+    words: req.body.description,
+    showYear: showyear,
+    month: date.getMonth(),
+    day: date.getDate(),
+    year: date.getFullYear(),
+  });
+  console.log(birthday);
+  birthday.save();
+  res.end();
+});
 
-
-
-app.listen(3001, () => { console.log("Server started succesfully") })
+app.listen(3001, () => {
+  console.log("Server started succesfully");
+});
